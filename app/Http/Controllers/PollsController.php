084b6e61d831e3c13d98e8bc0e0feaaf7c1c2b37
Raby NDIAYE <raby.ndiaye@paydunya.com>
2020-11-22 14:47:00 +0000

    add validation to inputs

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Choice;
 use App\Models\Poll;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class PollsController extends Controller
@@ -25,14 +26,23 @@ class PollsController extends Controller
         return view('polls.show', compact('poll'));
     }
 
-    public function store()
+    public $pollRules = [
+        'question_text' => 'required|max:255',
+        'question_choice_1' => 'required,',
+        'question_choice_2' => 'required',
+        'question_choice_3' => 'required',
+    ];
+
+    public function store(Request $request)
     {
-        $poll = Poll::create(['question_text' => request('question_text')]);
+        $validator = $request->validate($this->pollRules);
+
+        $poll = Poll::create(['question_text' => $request->get('question_text')]);
 
         if($poll){
             for ($i = 1 ;$i < 4; $i++){
                 $poll->choices()->create([
-                    'name' => request('question_choice_'.$i),
+                    'name' => $request->get('question_choice_'.$i),
                     'poll_id'=>  $poll->id
                 ]);
             }
--- resources/views/polls/create.blade.php
+++ resources/views/polls/create.blade.php
@@ -4,16 +4,28 @@
     <form method="POST" action="{{ route('polls.store') }}">
         @csrf
 
-        <div>
+        <div class="form-group">
             <label for="question_text">Question Text</label><br>
-            <textarea id="question_text" name="question_text" autofocus required></textarea>
+            <textarea id="question_text" name="question_text" autofocus required ></textarea>
+            @if ($errors->has('question_text'))
+                <span  style="color: red; display: block;">{{ $errors->first('question_text') }}</span>
+            @endif
         </div>
 
-        <div>
+        <div lass="form-group">
             <h3>Choices</h3>
             <input type="text" name="question_choice_1" placeholder="Enter choice" required><br>
+            @if ($errors->has('question_choice_1'))
+                <span  style="color: red; display: block;">{{ $errors->first('question_choice_1') }}</span>
+            @endif
             <input type="text" name="question_choice_2" placeholder="Enter choice" required><br>
+            @if ($errors->has('question_choice_2'))
+                <span style="color: red; display: block;">{{ $errors->first('question_choice_2') }}</span>
+            @endif
             <input type="text" name="question_choice_3" placeholder="Enter choice" required>
+            @if ($errors->has('question_choice_3'))
+                <span  style="color: red; display: block;">{{ $errors->first('question_choice_3') }}</span>
+            @endif
         </div>
 
         <br>
